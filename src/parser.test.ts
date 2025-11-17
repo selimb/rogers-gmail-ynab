9a7046670d8a928acf240c332f478bdf05696ea5
Selim Belhaouane <selim.belhaouane@garda.com>
2025-11-16 18:52:30 -0800

    v1.3

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -6,10 +6,8 @@ import { type ParsedRogersEmail, parseRogersEmail } from "./parser";
 const MESSAGE_FULL = `
 
 
-Hello MR TEST,
-
-Attempt of $33.08 was made on Oct 01, 2025 on your credit card ending in
-9999 at La Baguette Catering in REVELSTOKE.
+$69.59 on Nov 16, 2025 on your credit card ending in 9999. Details: SAVE ON
+FOODS #2245.
 
 *Learn how to manage account alerts.
 <https://rogersbank.com/en/help_centre_answers/managing_your_account#q174>*
@@ -22,79 +20,13 @@ Contact Us <https://www.rogersbank.com/en/contact_us> | Terms & Conditions
 <https://www.rogersbank.com/en/privacy> | rogersbank.com
 <https://www.rogersbank.com>
 Rogers Bank | 1 Mount Pleasant Road | Toronto ON | M4Y 2Y5
-This email was sent to you because you're a Rogers Bank customer. We value
+This email was sent to you because you’re a Rogers Bank customer. We value
 your privacy and security, and will never contact you for personal
 information, including any passwords or Multi-Factor Authentication (MFA)
 codes.
 `;
 
 describe(parseRogersEmail, () => {
-  it("parses a typical message", () => {
-    const message =
-      "Attempt of $33.08 was made on Oct 01, 2025 on your credit card ending in 9999 at La Baguette Catering in REVELSTOKE.";
-    const expected: ParsedRogersEmail = {
-      amount: 33.08,
-      date: "2025-10-01",
-      merchant: "La Baguette Catering",
-    };
-    expect(parseRogersEmail(message)).toEqual(expected);
-  });
-
-  it("handles merchant containing 'in'", () => {
-    const message =
-      "Attempt of $114.99 was made on Jan 15, 2024 on your credit card ending in 9999 at Dine in Cafe in NEW YORK.";
-    const expected: ParsedRogersEmail = {
-      amount: 114.99,
-      date: "2024-01-15",
-      merchant: "Dine in Cafe",
-    };
-    expect(parseRogersEmail(message)).toEqual(expected);
-  });
-
-  it("handles merchant containing 'at'", () => {
-    const message =
-      "Attempt of $42.00 was made on Mar 03, 2026 on your credit card ending in 9999 at Cafe at the Park in NEW YORK.";
-    const expected: ParsedRogersEmail = {
-      amount: 42,
-      date: "2026-03-03",
-      merchant: "Cafe at the Park",
-    };
-    expect(parseRogersEmail(message)).toEqual(expected);
-  });
-
-  it("handles merchant containing 'in' and 'at'", () => {
-    const message =
-      "Attempt of $55.55 was made on Aug 20, 2023 on your credit card ending in 4321 at Eat in at Noon in DOWNTOWN.";
-    const expected: ParsedRogersEmail = {
-      amount: 55.55,
-      date: "2023-08-20",
-      merchant: "Eat in at Noon",
-    };
-    expect(parseRogersEmail(message)).toEqual(expected);
-  });
-
-  it("handles merchant containing 'at' and 'in'", () => {
-    const message =
-      "Attempt of $55.55 was made on Aug 20, 2023 on your credit card ending in 4321 at Eat at Home in Kitchen in DOWNTOWN.";
-    const expected: ParsedRogersEmail = {
-      amount: 55.55,
-      date: "2023-08-20",
-      merchant: "Eat at Home in Kitchen",
-    };
-    expect(parseRogersEmail(message)).toEqual(expected);
-  });
-
-  it("handles(ish) location containing 'in'", () => {
-    const message =
-      "Attempt of $55.55 was made on Aug 20, 2023 on your credit card ending in 4321 at Eat at Home in Kitchen in Niagara in the lake.";
-    const expected: ParsedRogersEmail = {
-      amount: 55.55,
-      date: "2023-08-20",
-      merchant: "Eat at Home in Kitchen in Niagara",
-    };
-    expect(parseRogersEmail(message)).toEqual(expected);
-  });
-
   it("handles malformed messages gracefully", () => {
     const message = "This is not a valid message.";
     expect(parseRogersEmail(message)).toMatchInlineSnapshot(
@@ -104,7 +36,7 @@ describe(parseRogersEmail, () => {
 
   it("handles commas in the amount", () => {
     const message =
-      "Attempt of $1,234.56 was made on Nov 30, 2025 on your credit card ending in 5555 at Fancy Place in BIG CITY.";
+      "$1,234.56 on Nov 30, 2025 on your credit card ending in 9999. Details: Fancy Place.";
     const expected: ParsedRogersEmail = {
       amount: 1234.56,
       date: "2025-11-30",
@@ -115,9 +47,9 @@ describe(parseRogersEmail, () => {
 
   it("handles full message", () => {
     const expected: ParsedRogersEmail = {
-      amount: 33.08,
-      date: "2025-10-01",
-      merchant: "La Baguette Catering",
+      amount: 69.59,
+      date: "2025-11-16",
+      merchant: "SAVE ON FOODS #2245",
     };
     expect(parseRogersEmail(MESSAGE_FULL)).toEqual(expected);
   });
--- src/parser.ts
+++ src/parser.ts
@@ -5,12 +5,18 @@ export type ParsedRogersEmail = {
   date: string;
 };
 
-// Regex up to "at ... ."
 const REGEX =
-  /Attempt of \$(\S+) was made on ([A-Za-z]{3,} \d{2}, \d{4}) on your credit card ending in \d+ at (.+)\./;
+  /\$(\S+) on ([A-Za-z]{3,} \d{2}, \d{4}) on your credit card ending in \d+. Details: (.+)\./;
 
 export function parseRogersEmail(body: string): ParsedRogersEmail | Error {
-  body = body.replaceAll("\n", " ").replaceAll(/\s+/g, " ").trim();
+  // Trim all lines.
+  body = body
+    .trim()
+    .split("\n")
+    .map((s) => s.trim())
+    .join("\n");
+  // Join consecutive lines.
+  body = body.replaceAll(/\n([^\n])/g, (_match, s) => ` ${s}`);
 
   const match = REGEX.exec(body);
 
@@ -18,7 +24,7 @@ export function parseRogersEmail(body: string): ParsedRogersEmail | Error {
     return new Error("Message does not match regex.");
   }
 
-  const [, amountRaw, dateRaw, merchantAndLocation] = match;
+  const [, amountRaw, dateRaw, merchant] = match;
 
   const amount = Number.parseFloat(amountRaw.replaceAll(",", ""));
   if (Number.isNaN(amount)) {
@@ -30,23 +36,6 @@ export function parseRogersEmail(body: string): ParsedRogersEmail | Error {
     return new Error(`Invalid date: '${dateRaw}'.`);
   }
   const dateString = date.toISOString().split("T")[0];
-
-  // The general format is '$MERCHANT in $LOCATION', but either $MERCHANT or $LOCATION may contain ' in '.
-  // There's no way to disambiguate, but it's probably more likely that $MERCHANT contains ' in ' than $LOCATION.
-  const sep = " in ";
-  const parts = merchantAndLocation.split(sep);
-  if (parts.length < 2) {
-    return new Error(
-      `Missing ' in ' in merchant/location: '${merchantAndLocation}'.`,
-    );
-  }
-  let merchant: string;
-  // eslint-disable-next-line unicorn/prefer-ternary -- No.
-  if (parts.length === 2) {
-    merchant = parts[0];
-  } else {
-    merchant = parts.slice(0, -1).join(sep);
-  }
 
   return {
     amount,
